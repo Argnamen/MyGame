4a3d113bd88553ba7052e77023239dcb593ce85f
dmezrin <dmezrin@asomgames.ru>
2025-01-30 12:49:58 +1000

    Fix Vector

--- Assets/Scripts/Weapon/Projectile/Projectile.cs
+++ Assets/Scripts/Weapon/Projectile/Projectile.cs
@@ -20,6 +20,15 @@ public class Projectile : Character
     }
     public Projectile(int healt, float spead, float size, Weapon weapon, Vector2 startPos, Environment[] environments) : base(healt, spead, size, weapon, startPos, environments)
     {
+        /*
+        newPos = new Vector2(
+                 Target.Position.x < _startPos.x && Target.Position.x > 0 || Target.Position.x > _startPos.x && Target.Position.x < 0 ?
+                    _startPos.x - Target.Position.normalized.x * _spead :
+                    _startPos.x + Target.Position.normalized.x * _spead,
+                 Target.Position.y < _startPos.y && Target.Position.y > 0 || Target.Position.y > _startPos.y && Target.Position.y < 0 ?
+                    _startPos.y - Target.Position.normalized.y * _spead :
+                    _startPos.y + Target.Position.normalized.y * _spead);
+        */
     }
 
     public virtual Vector2 Move()
@@ -31,17 +40,7 @@ public class Projectile : Character
             newPos = Vector2.zero;
             isFirstStart = false;
 
-            newPos = new Vector2(
-                 Target.Position.x < _startPos.x && Target.Position.x > 0 || Target.Position.x > _startPos.x && Target.Position.x < 0 ?
-                    _startPos.x - Target.Position.normalized.x * _spead :
-                    _startPos.x + Target.Position.normalized.x * _spead,
-                 Target.Position.y < _startPos.y && Target.Position.y > 0 || Target.Position.y > _startPos.y && Target.Position.y < 0 ?
-                    _startPos.y - Target.Position.normalized.y * _spead :
-                    _startPos.y + Target.Position.normalized.y * _spead);
-
-            Vector = newPos;
-
-            UnityEngine.Debug.Log(_startPos + " " + Target.Position + " " + newPos);
+            Vector = (Target.Position - _startPos).normalized * _spead;
         }
 
         int world = _worldsMap.CorrectWorld;
@@ -62,38 +61,26 @@ public class Projectile : Character
 
         foreach (var env in Environments)
         {
-            if (env != null)
+            if (env != null && (Vector2.Distance(newPos, env.transform.position) < env.Enemy.Size))
             {
-                if (env != null && (Vector2.Distance(newPos, env.transform.position) < env.Enemy.Size))
+                // Пересчёт вектора движения
+                if (Mathf.Abs(env.transform.position.x - newPos.x) <= Mathf.Abs(env.transform.position.y - newPos.y))
                 {
-                    // Пересчёт вектора движения
-                    if (Mathf.Abs(env.transform.position.x - newPos.x) <= Mathf.Abs(env.transform.position.y - newPos.y))
-                    {
-                        Vector = Vector2.Reflect(Vector, Vector2.right); // Отражение по оси X
-                    }
-                    else
-                    {
-                        Vector = Vector2.Reflect(Vector, Vector2.up); // Отражение по оси Y
-                    }
+                    Vector = Vector2.Reflect(Vector, Vector2.right); // Отражение по оси X
+                }
+                else
+                {
+                    Vector = Vector2.Reflect(Vector, Vector2.up); // Отражение по оси Y
                 }
             }
         }
 
-        /*
-        newPos = new Vector2(
-                Vector.x < _startPos.x && Vector.x > 0 || Vector.x > _startPos.x && Vector.x < 0 ?
-                    _startPos.x - Vector.normalized.x * _spead :
-                    _startPos.x + Vector.normalized.x * _spead,
-                Vector.y < _startPos.y && Vector.y > 0 || Vector.y > _startPos.y && Vector.y < 0 ?
-                    _startPos.y - Vector.normalized.y * _spead :
-                    _startPos.y + Vector.normalized.y * _spead);
-        */
-
-        newPos = _startPos + Vector.normalized * _spead;
+        newPos = _startPos + Vector * _spead;
 
         _startPos = newPos;
 
         return newPos;
     }
+
 
 }
